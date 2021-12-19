6a76aa1c9bbbc49e9e292cfc4ac16a345ccb06e5
rony20191 <ronieresilva18@gmail.com>
2021-12-19 17:12:14 -0300

    [NEW] first test

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        <h1 className={styles.title}>
+          <a href="about">about</a>
+        </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
